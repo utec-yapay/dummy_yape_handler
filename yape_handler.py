1831516f2f98021802a450347f3476b4e91ab697
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2019-06-28 04:07:27 -0500

    Prettify response and run on port 5001

--- yape_handler.py
+++ yape_handler.py
@@ -1,8 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import json
 app = Flask(__name__)
 
-global last_report
+last_report = {}
 
 @app.route('/handler/payments', methods=['POST', 'GET'])
 def managePayments():
@@ -10,8 +10,8 @@ def managePayments():
     if request.method == 'POST':
         last_report = json.loads(request.data)
         # last_report = request.form
-        return json.dumps({"success": 200})
-    return json.dumps(last_report, indent=4)
+        return jsonify(success=200)
+    return jsonify(last_report)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5001)
