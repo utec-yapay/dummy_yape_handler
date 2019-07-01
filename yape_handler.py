ac1db4f127dd7fedaca67f1d6995a7fffae2119e
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2019-06-30 19:53:49 -0500

    Store all reports instead of last one

--- yape_handler.py
+++ yape_handler.py
@@ -2,13 +2,13 @@ from flask import Flask, request, jsonify
 import json
 app = Flask(__name__)
 
-last_report = {}
+last_report = []
 
 @app.route('/handler/payments', methods=['POST', 'GET'])
 def managePayments():
     global last_report
     if request.method == 'POST':
-        last_report = json.loads(request.data)
+        last_report.append(json.loads(request.data))
         # last_report = request.form
         return jsonify(success=200)
     return jsonify(last_report)
